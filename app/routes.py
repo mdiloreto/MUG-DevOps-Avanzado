a5180e10535f14fbc4a6164b4e8816788fa8a4af
Mateo Di Loreto <101841200+mdiloreto@users.noreply.github.com>
2024-11-27 23:52:23 -0300

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, send_file, render_template, send_from_directory
+from flask import Flask, request, jsonify, send_file, render_template
 from app import app
 from app.services.scraper import Scraper
 from app.services.translator_azureai import Translator_azure
@@ -13,18 +13,9 @@ import os
 logging.basicConfig(level=logging.DEBUG)
 output_dir = os.getenv("FLASK_OUTPUT_DIR", "/app/outputs") ## Default for local tests:"/app/outputs" ||  "/home" for Linux App Service Web App
 
-# Serve only img.png from the mounted storage
-@app.route('/mnt/sa_mount/img.png')
-def serve_image():
-    storage_path = '/mnt/sa_mount'
-    filename = 'img.png'  # Hardcoded filename
-    return send_from_directory(storage_path, filename)
-
-
 @app.route('/')
 def home():
     return render_template("index.html")
-    
 
 @app.route('/api/translate', methods=['POST'])
 def transcribe_video():
